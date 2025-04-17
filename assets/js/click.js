23da35b1ebd8e63ccc26bc8702fb98ea8637094a
Donghoon Shin <dhoon@uw.edu>
2025-04-16 19:18:04 -0700

    Create click.js

---
+++ assets/js/click.js
@@ -0,0 +1,13 @@
+document.addEventListener("click", function (event) {
+  const id = event.target.id;
+  if (id) {
+    const rect = event.target.getBoundingClientRect();
+    const boundingBox = {
+      top: rect.top,
+      left: rect.left,
+      width: rect.width,
+      height: rect.height
+    };
+    window.parent.postMessage({ clickedId: id, boundingBox }, "*");
+  }
+});
