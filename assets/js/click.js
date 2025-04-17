097be53a3780294312dce25a8166c8849e2489a0
Donghoon Shin <dhoon@uw.edu>
2025-04-16 22:57:20 -0700

    Update click.js

--- assets/js/click.js
+++ assets/js/click.js
@@ -1,17 +1,13 @@
 document.addEventListener("click", function (event) {
   const id = event.target.id;
-  console.log(id)
-  alert(id)
   if (id) {
     const rect = event.target.getBoundingClientRect();
-    console.log(rect)
     const boundingBox = {
       top: rect.top,
       left: rect.left,
       width: rect.width,
       height: rect.height
     };
-    console.log(boundingBox)
-    window.parent.postMessage({ type: "click-event", clickedId: id, boundingBox }, "*");
+    window.parent.postMessage({ type: "click-event", clickedId: id, boundingBox: boundingBox }, "*");
   }
 });
