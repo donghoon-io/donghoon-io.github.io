7b9bc4d93bd3531a72d89b002bef8ced3c49c9b4
Donghoon Shin <dhoon@uw.edu>
2025-05-07 03:31:58 -0700

    Update click.js

--- assets/js/click.js
+++ assets/js/click.js
@@ -15,24 +15,17 @@ document.addEventListener("click", function (event) {
 });
 
 document.addEventListener("mousedown", function (event) {
-  console.log("moused down");
 
   const target = event.target;
-  const boundingBox = target.getBoundingClientRect();
 
   window.parent.postMessage(
     {
       type: "mouse-down",
-      id: target.id,
-      boundingBox: {
-        top: boundingBox.top,
-        left: boundingBox.left,
-        width: boundingBox.width,
-        height: boundingBox.height,
-      },
+      id: target.id
     },
     "*"
   );
+  console.log("moused down");
 });
 
 setInterval(() => {
