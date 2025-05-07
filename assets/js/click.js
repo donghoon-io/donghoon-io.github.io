c39fb130983bc9e67b21bcb824eb4913b844556e
Donghoon Shin <dhoon@uw.edu>
2025-05-06 22:08:31 -0700

    Update click.js

--- assets/js/click.js
+++ assets/js/click.js
@@ -17,19 +17,4 @@ document.addEventListener("click", function (event) {
 setInterval(() => {
   const html = document.documentElement.outerHTML;
   window.parent.postMessage( { type: "screenshot-event", html: html }, "*" );
-
-  const currentScrollTop = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
-  const currentScrollLeft = window.scrollX || document.documentElement.scrollLeft || document.body.scrollLeft;
-  const currentClientHeight = window.innerHeight;
-  const currentClientWidth = window.innerWidth;
-
-  console.log(currentScrollTop)
-
-  window.parent.postMessage( {
-      type: "preview-scroll",
-      scrollTop: currentScrollTop,
-      scrollLeft: currentScrollLeft,
-      clientHeight: currentClientHeight,
-      clientWidth: currentClientWidth
-  }, "*" );
 }, 1000);
